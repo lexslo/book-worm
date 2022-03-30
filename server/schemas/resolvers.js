5370ff0d651c90a0145e4499833f22db9e4aeb53
Sparkle Party <lexi.slovik@gmail.com>
2022-03-30 14:23:04 -0700

    update types in typedefs, set up resolvers with shell functions

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,40 @@
+const { User, Book } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return 'Hello World!';
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('savedBooks');
+    
+                return userData;
+            }
+    
+            throw new AuthenticationError('Not logged in');
+        }
+    },
+
+    Mutation: {
+        login: async (parent, { email, password }) => {
+
+        },
+
+        addUser: async (parent, args) => {
+
+        },
+
+        saveBook: async (parent, args) => {
+
+        },
+
+        removeBook: async (parent, args) => {
+            
         }
     }
+    
 };
 
 module.exports = resolvers;
