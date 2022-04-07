8451e7adfcf67b24f7e60114e34c82413db4db42
Sparkle Party <lexi.slovik@gmail.com>
2022-04-06 19:59:04 -0700

    update authorization check on front end in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,25 +6,33 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
-const httpLink = createHttpLink({
-  uri: '/graphql'
-});
+// const httpLink = createHttpLink({
+//   uri: '/graphql'
+// });
 
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
+// const authLink = setContext((_, { headers }) => {
+//   const token = localStorage.getItem('id_token');
 
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : ''
-    }
-  }
-});
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : ''
+//     }
+//   }
+// });
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache()
-})
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+    
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql'
+});
 
 function App() {
   return (
